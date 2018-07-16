aad13e11897602e34e1d38e082bc6ceb16d6c641
tcknapp <trevor.knapp.1991@gmail.com>
2018-07-16 13:33:49 -0700

    initial

---
+++ app.js
@@ -0,0 +1,13 @@
+
+//Firebase ver
+
+var config = {
+        apiKey: "AIzaSyCxPsBIThIwPZGKlBPuiPZaF_CqsfcQmHs",
+        authDomain: "fir-1-69ad9.firebaseapp.com",
+        databaseURL: "https://fir-1-69ad9.firebaseio.com",
+        projectId: "fir-1-69ad9",
+        storageBucket: "fir-1-69ad9.appspot.com",
+        messagingSenderId: "220023988461"
+      };
+      firebase.initializeApp(config);
+    
