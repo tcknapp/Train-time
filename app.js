3ad7088ce54a47089e1590e98930685f3d9b2b79
tcknapp <trevor.knapp.1991@gmail.com>
2018-07-19 20:56:42 -0700

    working firebase

--- app.js
+++ app.js
@@ -10,4 +10,43 @@ var config = {
         messagingSenderId: "220023988461"
       };
       firebase.initializeApp(config);
-    
+
+    var database = firebase.database();
+
+    //variables for form
+    var trainName= "";
+    var destination= "";
+    var firstTrainTime= "";
+    var frequency= "";
+
+
+    $('#submit').on('click', function(event){
+        event.preventDefault();
+        console.log(event);
+
+        trainName = $('#trainName').val().trim();
+        destination = $('#destination').val().trim();
+        firstTrainTime = $('#firstTrainTime').val().trim();
+        frequency = $('#frequency').val().trim();
+
+        database.ref().push({
+            train: trainName,
+            destination : destination,
+            trainTime: firstTrainTime,
+            frequency: frequency,
+        });
+
+        return false;
+    }); 
+
+    database.ref().on("child_added", function(childSnapshot) {
+
+    });
+
+    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+        // Change the HTML to reflect
+        $("#trainName").text(snapshot.val().trainName);
+        $("#destination").text(snapshot.val().destination);
+        $("#firstTrainTime").text(snapshot.val().firstTrainTime);
+        $("#frequency").text(snapshot.val().frequency);
+      });
